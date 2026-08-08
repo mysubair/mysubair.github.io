a5d5beaa09ec41e3d06d0433904bd591d7ec8c80
mysubair <yousufmuhammedkpm+1@gmail.com>
2026-08-08 23:09:02 +0300

    Update research-site.tsx

--- components/research-site.tsx
+++ components/research-site.tsx
@@ -237,7 +237,7 @@ yousufmuhammedkpm@gmail.com</a><a
     <path d="M20.45 20.45h-3.56v-5.57c0-1.33-.03-3.04-1.85-3.04-1.85 0-2.14 1.45-2.14 2.94v5.67H9.35V9h3.41v1.56h.05c.48-.9 1.64-1.85 3.37-1.85 3.61 0 4.27 2.37 4.27 5.45v6.29zM5.34 7.43a2.06 2.06 0 1 1 0-4.12 2.06 2.06 0 0 1 0 4.12zM6.12 20.45H2.56V9h3.56v11.45z" />
   </svg>
 </span>
-<span style={{ marginLeft: '4px' }}>LinkedIn</span>
+<span style={{ marginLeft: '2px' }}>LinkedIn</span>
 
 </a><a
   href="https://orcid.org/0009-0007-2452-0181?lang=en"
